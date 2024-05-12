d679a6644ae1aa99af3d643fe167f50b3dcf32cb
danilo <140023124+Danil04ndre@users.noreply.github.com>
2024-05-12 17:27:27 -0500

    agregar proyecto ecommerce2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,10 +6,12 @@ import videoCrud from "../assets/crn.mp4";
 import projectMovie from "../assets/project-movie.png";
 import projectMovie2 from "../assets/project-movie2.png";
 
+import ecommerce from "../assets/ecommerce.png";
+import ecommerce2 from "../assets/ecommerce2.png";
+
 import react from "../assets/react.png";
 import node from "../assets/node.png";
 import mysql from "../assets/mysql.png";
-
 import "../css/Projects.css";
 
 const Projects = () => {
@@ -26,6 +28,48 @@ const Projects = () => {
   return (
     <section className="content-projects" id="projects">
       <h2>Mis Proyectos</h2>
+      <div className="item-project">
+        <div className="info">
+          <h3>Ecommerce 2.0</h3>
+          <p>
+            Un proyecto mejorado despues de aprender nuevas tecnologias como
+            redux, jwt entre otras validaciones y buenas practicas
+          </p>
+          <figure>
+            <img src={react} alt="React" />
+            <img src={node} alt="JavaScript" />
+            <img
+              src="https://logowik.com/content/uploads/images/redux.jpg"
+              alt="JavaScript"
+              className="mysql"
+            />
+            <img src={mysql} alt="JavaScript" className="mysql" />
+          </figure>
+          <a
+            href="https://github.com/Danil04ndre/ecommerce-react-nodejs-db"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Repositorio <i className="fa-brands fa-github"></i>
+          </a>
+          <a
+            href="https://ecommerce-three-sooty.vercel.app/"
+            className="link-page"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ver pagina
+          </a>
+        </div>
+        <div className="video image-project">
+          <img
+            className="a"
+            src={!movieImage ? ecommerce : ecommerce2}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+          />
+        </div>
+      </div>
       <div className="item-project">
         <div className="info">
           <h3>Ecommerce</h3>

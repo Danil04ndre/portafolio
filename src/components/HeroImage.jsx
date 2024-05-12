464bbc64b5b74ccfabcd36c51ff661a3c538f255
danilo <140023124+Danil04ndre@users.noreply.github.com>
2024-05-12 17:05:05 -0500

    cambiar info

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -7,7 +7,7 @@ const HeroImage = () => {
         <div className="info">
           <p>Hola, me presento:</p>
           <h1>DANILO VILLAVICENCIO</h1>
-          <p className="p">Programador</p>
+          <p className="p">Desarrollador Fullstack</p>
           <a
             href="https://drive.google.com/file/d/1QtUAHpdgu0EIsxS8EMyTNubcAoK5R1iL/view?usp=sharing"
             target="_blank"
